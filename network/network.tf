26a282722c292915bd94fe8f3c39d94c25971d25
nexo8937 <marsel8937@gmail.com>
2025-09-08 16:11:24 +0400

    fixing vpc

--- network/network.tf
+++ network/network.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "working" {}
 
 #VPC
 resource "aws_vpc" "vpc" {
-  count      = var.vpc_cidr_block != null ? 1 : 0
+  #  count      = var.vpc_cidr_block != null ? 1 : 0
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.app}-vpc-${var.env}"

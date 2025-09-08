27b937ed80310f5176813a86533067e15051aaba
nexo8937 <marsel8937@gmail.com>
2025-09-08 16:08:20 +0400

    creating vpc

---
+++ network/network.tf
@@ -0,0 +1,11 @@
+#data
+data "aws_availability_zones" "working" {}
+
+#VPC
+resource "aws_vpc" "vpc" {
+  count      = var.vpc_cidr_block != null ? 1 : 0
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.app}-vpc-${var.env}"
+  }
+}
---
+++ network/variables.tf
@@ -0,0 +1,14 @@
+variable "app" {
+  description = "Application Name"
+  default     = ""
+}
+
+variable "env" {
+  default = ""
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = null
+}
